849624379007e4cff1d15ab6fb06c2e4a744bdd8
leeyc924 <leeyc924@naver.com>
2023-08-09 23:12:39 +0900

    Modify jest init

--- jest.setup.js
+++
@@ -1,6 +0,0 @@
-import '@testing-library/jest-dom';
-import { server } from './src/api/node';
-
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
